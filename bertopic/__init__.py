efe96f0ee70ed732a45d823154e00de2da7c94a0
Ian Randman <ixr5487@rit.edu>
2024-07-03 11:01:45 -0700

    Fix package name (#1)

--- bertopic/__init__.py
+++ bertopic/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import version
 
 from bertopic._bertopic import BERTopic
 
-__version__ = version("bertopic")
+__version__ = version("semandex-bertopic")
 
 __all__ = [
     "BERTopic",
